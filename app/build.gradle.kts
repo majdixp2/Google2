24c27f554a7dacb220eee7b557274747d8725ff0
majdixp2 <majdixp2@gmail.com>
2026-09-07 09:27:22 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,10 @@ android {
   }
 }
 
+base {
+  archivesName.set("Addad")
+}
+
 // Configure the Secrets Gradle Plugin to use .env and .env.example files
 // to match the convention used in Web projects.
 secrets {
